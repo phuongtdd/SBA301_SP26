372db2eb2ae94aedf77f79c8c3a05320006a276f
phuongtddd <phuongtdd3711@gmail.com>
2026-01-06 10:01:11 +0700

    Update ex1

--- Slot1/ex1/src/components/Footer.jsx
+++ Slot1/ex1/src/components/Footer.jsx
@@ -1,7 +1,25 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p>© 2024 My App. All rights reserved.</p>
-    </footer>
-  )
-}
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+function Footer() {
+    return (
+        <footer className="bg-light text-center py-4 mt-auto">
+        <Container fluid>
+            <Row className="align-items-center">
+                <Col xs={2}>
+                    <Image src="/images/work.jfif" alt="Author Avatar" className="rounded-circle" style={{ width: '60px', height: '60px', objectFit: 'cover' }} />
+                </Col>
+                <Col xs={8}>
+                    <h5>Tác giả: &copy; phuongtdd</h5>
+                    <small>All rights reserved.</small>
+                </Col>
+                <Col xs={2}>
+                    <a href="mailto:phuongtddde180212@fpt.edu.vn">phuongtddde180212@fpt.edu.vn</a>    
+                </Col>
+            </Row>
+        </Container>
+        </footer>
+    );
+}   
+export default Footer;
