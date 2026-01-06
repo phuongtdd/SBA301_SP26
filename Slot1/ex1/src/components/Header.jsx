1d79bf6343b1e23b0368f0d83c37ebb77fa6d5ad
phuongtddd <phuongtdd3711@gmail.com>
2026-01-06 09:55:53 +0700

    SBA301

---
+++ Slot1/ex1/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <h1>Welcome to My App</h1>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </header>
+  );
+}
